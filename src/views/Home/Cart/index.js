4110caf98000f2ad2adf79814b1c3e1298f25b51
IDLbb <494435175@qq.com>
2019-03-04 17:38:45 +0800

    修改了购物车页面-lzy

--- src/views/Home/Cart/index.js
+++ src/views/Home/Cart/index.js
@@ -6,8 +6,61 @@ import './index.css';
 class Cart extends React.Component{
   render(){
     return(
-      <div>
-          <div>Cart</div>
+      <div className="A">
+        <header>购物车</header>
+        <div className="nav">
+          <p><i className="iconfont icon-gwc">购物车还没有商品赶紧选购吧！</i></p>
+          <p><a href="#">去逛逛</a></p>
+        </div>
+        <div className="mian">
+          <p>热销推荐</p>
+          <div className="mian_t">
+            <ul>
+                <li>
+                    <a href="#">
+                      <img src="https://res.vmallres.com/pimages/product/2601010086605//428_428_1550815661328mp.png" alt="" />
+                      <h3>HUAWEI | nova</h3>
+                      <h2>￥3099</h2>
+                    </a>
+                </li>
+                <li>
+                      <a href="#">
+                        <img src="https://res.vmallres.com/pimages/product/6901443280988//428_428_1545877216682mp.png" alt="" />
+                        <h3>荣耀V20</h3>
+                        <h2>￥2999</h2>
+                      </a>
+                </li>
+                <li>
+                        <a href="#">
+                          <img src="https://res.vmallres.com/pimages/product/6901443260232//428_428_1539230426520mp.png" alt="" />
+                          <h3>HUAWEI|Mate 20</h3>
+                          <h2>￥3999</h2>
+                        </a>
+                </li>
+                <li>
+                          <a href="#">
+                            <img src="https://res.vmallres.com/pimages/product/6901443232413//428_428_1535358175728mp.jpg" alt="" />
+                            <h3>荣耀10 A|摄影手机</h3>
+                            <h2>￥2199</h2>
+                          </a>
+                </li>
+                <li>
+                            <a href="#">
+                              <img src="https://res.vmallres.com/pimages/product/6901443262182//428_428_1542769704670mp.png" alt="" />
+                              <h3>HUAWEI|Mate 20 X</h3>
+                              <h2>￥4999</h2>
+                            </a>
+                </li>
+                <li>
+                              <a href="#">
+                                <img src="https://res.vmallres.com/pimages/product/6901443272709//428_428_1540987820121mp.png" alt="" />
+                                <h3>荣耀8X</h3>
+                                <h2>￥1399</h2>
+                              </a>
+                </li>
+            </ul>
+          </div>
+        </div>
       </div>
     )
   }
