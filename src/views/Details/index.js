6d07abcc575f89ad146fd852fc8aa0980a855b6d
IDLbb <494435175@qq.com>
2019-03-07 11:48:16 +0800

    casqcw

--- src/views/Details/index.js
+++ src/views/Details/index.js
@@ -1,11 +1,31 @@
+import { Carousel, WingBlank } from 'antd-mobile';
+import 'antd-mobile/dist/antd-mobile.css';
 import React, { Component } from 'react';
 import './index.less';
-
 class Details extends Component {
   render() {
     return (
       <div className="details">
-      详情页面</div>
+        <div className="lzy-swiper">
+
+
+
+
+             <WingBlank>
+                <Carousel
+                  autoplay={true}
+                  infinite
+                  dots={true}
+                >
+                  <img src="https://res.vmallres.com/pimages//sale/2019-02/ugfxv1s1vEseKNice2ok.jpg" alt="" />
+                  <img src="https://res.vmallres.com/pimages//sale/2019-02/6Km8n1Rwe94ttWlNKtsE.jpg" alt="" />
+                  <img src="https://res.vmallres.com/pimages//sale/2019-02/8Kl2w0EsqEH45Nx6e74B.jpg" alt="" />
+                  <img src="https://res.vmallres.com/pimages//sale/2019-02/EnaGwaeJbrWGfyhlnzJo.png" alt=""/>
+                </Carousel>
+              </WingBlank>
+
+        </div>
+      </div>
     )
   }
 }
